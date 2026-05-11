0923a8555e725a02171e0fe20ef00eb851bcec66
AngelinaGKong <konga2@carleton.edu>
2026-05-11 18:08:58 -0400

    got the jupyter cleaned country

--- Data/createtable.sql
+++ Data/createtable.sql
@@ -1,33 +1,35 @@
-DROP TABLE IF EXISTS water_country_cleaned_again_again;
-CREATE TABLE water_country_cleaned_again_again (
+DROP TABLE IF EXISTS water_country_cleaned_again_again_with_utf8;
+CREATE TABLE water_country_cleaned_again_again_with_utf8 (
     country TEXT,
-    year_n REAL,
-    population_n REAL,
-    urban_percent REAL,
-    basic_percent REAL,
-    limited REAL,
-    unimproved REAL,
-    safely_managed REAL,
-    accessible REAL,
-    available REAL,
-    no_contamination REAL,
-    piped REAL,
-    not_piped REAL
+    year_n TEXT,
+    population_n TEXT,
+    urban_percent TEXT,
+    basic_percent TEXT,
+    limited TEXT,
+    unimproved TEXT,
+    safely_managed TEXT,
+    accessible TEXT,
+    available TEXT,
+    no_contamination TEXT,
+    piped TEXT,
+    not_piped TEXT
 );
 
 DROP TABLE IF EXISTS water_region;
 CREATE TABLE water_region (
     country TEXT,
-    year_n REAL,
-    population_n REAL,
-    urban_percent REAL,
-    basic_percent REAL,
-    limited REAL,
-    unimproved REAL,
-    safely_managed REAL,
-    accessible REAL,
-    available REAL,
-    no_contamination REAL,
-    piped REAL,
-    not_piped REAL
+    year_n TEXT,
+    population_n TEXT,
+    urban_percent TEXT,
+    basic_percent TEXT,
+    limited TEXT,
+    unimproved TEXT,
+    safely_managed TEXT,
+    accessible TEXT,
+    available TEXT,
+    no_contamination TEXT,
+    piped TEXT,
+    not_piped TEXT
 );
+
+DROP TABLE IF EXISTS water_country_cleaned_again_again_with_comma_dil;

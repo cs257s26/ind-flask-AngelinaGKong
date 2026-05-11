7dfa86a787df8108d4f24c9d47f14b7c8a2dcdfe
Angelina Kong <konga2@carleton.edu>
2026-05-10 19:18:44 -0500

    finished createtable.sql

--- Data/createtable.sql
+++ Data/createtable.sql
@@ -1,6 +1,35 @@
 DROP TABLE IF EXISTS water_country;
 CREATE TABLE water_country (
     country TEXT,
-    year REAL,
-    population REAL
+    year_n REAL,
+    population_n REAL,
+    urban_percent REAL,
+    basic_percent REAL,
+    limited REAL,
+    unimproved REAL,
+    surface_water REAL,
+    safely_managed REAL,
+    accessible REAL,
+    available REAL,
+    no_contamination REAL,
+    piped REAL,
+    not_piped REAL
+);
+
+DROP TABLE IF EXISTS water_region;
+CREATE TABLE water_region (
+    country TEXT,
+    year_n REAL,
+    population_n REAL,
+    urban_percent REAL,
+    basic_percent REAL,
+    limited REAL,
+    unimproved REAL,
+    surface_water REAL,
+    safely_managed REAL,
+    accessible REAL,
+    available REAL,
+    no_contamination REAL,
+    piped REAL,
+    not_piped REAL
 );
